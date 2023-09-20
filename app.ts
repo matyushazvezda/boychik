7d3521526f2b22e496e8a56643e9e644f7abca8c
matyushazvezda <matyushazvezda@yandex.ru>
2023-09-20 11:12:35 +0800

    no commit

--- app.ts
+++ app.ts
@@ -1,4 +1,5 @@
 const currentDate = new Date();
+
 const newYearDate = new Date(currentDate.getFullYear(), 11, 31);
 const timeDiff = newYearDate.getTime() - currentDate.getTime();
 const daysRemaining = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
